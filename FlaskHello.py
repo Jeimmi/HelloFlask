894ed8077357c47f51355b39ca1db0a206bb750f
Jeimmi <jeimmi.gomez@gmail.com>
2016-06-01 21:39:11 -0700

    Users and post

--- FlaskHello.py
+++ FlaskHello.py
@@ -8,3 +8,13 @@ def index():
 @app.route('/FlaskTalks')
 def hello_world():
     return 'Hello, World of Flask!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
