e8327c4e9eead21caacff1add98819ff5fcdc424
Jeimmi <jeimmi.gomez@gmail.com>
2016-06-01 21:06:09 -0700

    Adding an Index

--- FlaskHello.py
+++ FlaskHello.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/FlaskTalks')
 def hello_world():
     return 'Hello, World of Flask!'
